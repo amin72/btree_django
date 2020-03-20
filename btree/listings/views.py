de70d135508f78ee948a154169d04e47b058b02c
Amin <asaa47721@gmail.com>
2020-03-20 08:04:36 +0330

    define listings, listing and search views

--- btree/listings/views.py
+++ btree/listings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def listings(request):
+    context = {
+        'listings_active': True
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+
+def listing(request, listing_id):
+    context = {}
+    return render(request, 'listings/listing.html', context)
+
+
+
+def search(request):
+    context = {}
+    return render(request, 'listings/search.html', context)
