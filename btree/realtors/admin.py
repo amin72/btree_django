942f740abd7702a87699d3bab6b5913d78388c5f
Amin <asaa47721@gmail.com>
2020-03-20 13:57:55 +0330

    define RealtorAdmin model admin for Realtor model

--- btree/realtors/admin.py
+++ btree/realtors/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . models import Realtor
 
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'hire_date']
+    search_fields = ['name']
+    list_per_page = 20
+
+
+admin.site.register(Realtor, RealtorAdmin)
