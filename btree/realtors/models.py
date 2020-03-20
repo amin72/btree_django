625d1d3bf723e11b1404728c46f8d47c3553ef09
Amin <asaa47721@gmail.com>
2020-03-20 12:53:24 +0330

    remove default value and set blank=True

--- btree/realtors/models.py
+++ btree/realtors/models.py
@@ -8,7 +8,7 @@ class Realtor(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=50)
     is_mvp = models.BooleanField(default=False)
-    hire_date = models.DateTimeField(auto_now_add=True)
+    hire_date = models.DateTimeField(blank=True)
 
     def __str__(self):
         return self.name
