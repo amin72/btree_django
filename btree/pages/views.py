3c9b9b74613f1f092f6640d748575aebc4c4e95f
Amin <asaa47721@gmail.com>
2020-03-21 18:51:53 +0430

    send realtors and mvp_realtors to about.html

--- btree/pages/views.py
+++ btree/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
@@ -15,7 +16,13 @@ def index(request):
 
 
 def about(request):
+    # get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)
+    
     context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors,
         'about_active': True
     }
     return render(request, "pages/about.html", context)
