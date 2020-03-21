1dca754c0156d48ffdb882128271ca703a1b4781
Amin <asaa47721@gmail.com>
2020-03-21 09:27:12 +0430

    send listings to index.html template

--- btree/pages/views.py
+++ btree/pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from listings.models import Listing
 
 
 def index(request):
+    listings = Listing.objects.filter(is_published=True) \
+        .order_by('-list_date')[:3]
+    
     context = {
+        'listings': listings,
         'home_active': True
     }
     return render(request, "pages/index.html", context)
