4f32f8a7278de84da7557235e46336b04d343844
Amin <asaa47721@gmail.com>
2020-03-22 21:33:07 +0430

    define Contact model. makemigrations

--- btree/contacts/models.py
+++ btree/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    listing = models.CharField(max_length=200)
+    listing_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.PositiveIntegerField(blank=True)
+
+    def __str__(self):
+        return self.name
