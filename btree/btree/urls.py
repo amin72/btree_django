94f649587575a76357db1838f3b7503cb60b4773
Amin <asaa47721@gmail.com>
2020-03-22 20:16:41 +0430

    add accounts urls to project urls

--- btree/btree/urls.py
+++ btree/btree/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('listings/', include('listings.urls')),
     path('', include('pages.urls')),
 ]
